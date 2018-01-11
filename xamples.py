a66bd8831a8cb03c584033d824306164810fe75d
ecaminero <caminero.edwin@gmail.com>
2018-01-11 09:42:30 -0300

    adding test with json

--- xamples.py
+++ xamples.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 txtRDD = sc.textFile('/user/cloudera/spark/emol_economia.txt')
 words = txtRDD.flatMap(lambda line: line.split())
 txt = words.map(lambda w: (w, 1))
@@ -12,6 +15,8 @@ def saludo(nombre):
   return 'hola  %s' % nombre
 s = saludo('veronica')  # ---> 'hola veronica'
 
+####
+l = ['00001 s1:s2:s3', '00002 s1:s2:s3', '00003 s1:s2:s3']
 
 ########################
 (lambda nombre: 'hola  %s' % nombre)('vero')
