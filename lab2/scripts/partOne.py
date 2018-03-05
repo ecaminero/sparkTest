71f5095c2a55f0d4bfe882b50d329ebc96a0d73b
ecaminero <caminero.edwin@gmail.com>
2018-03-04 23:37:50 -0300

    Adding code for part one

--- lab2/scripts/partOne.py
+++ lab2/scripts/partOne.py
@@ -4,7 +4,6 @@
 import time
 from pyspark import SparkContext
 
-t1 = time.time()
 sc = SparkContext()
 
 def generateData(dataJoined):
@@ -12,55 +11,110 @@ def generateData(dataJoined):
 
 def joinData(genedateData, dataset):
   return genedateData.join(dataset)
-# Extraemos la información o el dataset que esta en hdfs para realizar las consultas
+
+def getBestRaiting(raiting):
+  return raiting.filter(lambda element: element[1]>=100)
+
+def ordering(data, ascending):
+  return data.sortBy(lambda x: x[1], ascending = ascending)
+
+def averageData(a, b):
+  print(a, b)
+  return a+b
+
+
+# Extraemos la información del dataset que esta en hdfs para realizar las consultas
 # Generamos los RDD's de movies y ratings
-movieText = sc.textFile('/user/cloudera/lab-spark-2/movies.dat')
-ratingText = sc.textFile('/user/cloudera/lab-spark-2/ratings.dat')
+movieRDD = sc.textFile('/user/cloudera/lab-spark-2/movies.dat')
+ratingRDD = sc.textFile('/user/cloudera/lab-spark-2/ratings.dat')
 
 # Generamos una Cache de los RDD para obtener velocidad
-movieText.cache()
-ratingText.cache()
+# movieRDD.cache()
+# ratingRDD.cache()
 
-# Hacemos un split de los datos que estan en el RDD ratingText para obtener la información
+# Hacemos un split de los datos que estan en el RDD ratingRDD para obtener la información
 #  user_id, movie_id,  raiting,   timestamp
 # [[u'1',   u'1193',    u'5',     u'978300760']]
-ratingsData = ratingText.map(lambda line: line.split(';'))
+ratingsData = ratingRDD.map(lambda line: line.split(';'))
 
-# Generamos un RDD con el ID de la pelicula y 1 como primer voto
+# Generamos un RDD con el ID de la pelicula y el raiting
 #      movie_id, rating
 # Ej [(u'1193',    1)]
 pairsRating = ratingsData.map(lambda vec: (vec[1], 1))
 
 # agrupamos por movie_id y sumamos los ratings de cada uno
-# (key, [value, value])
+# Ej: (key, [value, value])
 # Generamos un nuevo RDD con (movie_id, total_raitings)
 countRating = pairsRating.reduceByKey(lambda a,b: a+b)
 
-# Hacemos un split de los datos que estan en el RDD movieText para obtener la información
-movieData = movieText.map(lambda line: line.split(';'))
+# Hacemos un split de los datos que estan en el movieRDD para obtener la información
+movieData = movieRDD.map(lambda line: line.split(';'))
 
 # Generamos un RDD con la lista de los ids de las peliculas con sus nombres
-# (movie_id, title)
+# Ej: (movie_id, title)
 pairsMovie = movieData.map(lambda vec: (vec[0], vec[1]))
 
-# Ordenamos las peliculas de forma descendente
-sortedData = countRating.sortBy(lambda x: x[1], ascending = False)
+# Ordenamos las peliculas de forma descendente para obtener las  mejores puntuaciones
+sortedData = ordering(countRating, False)
 
-# Generamos una partición en el dataset con las 20 mejores peliculas, generalmente entre 2-4 particiones en cada CPU
-bestTwenty = sc.parallelize(sortedData.take(20))
-bestHundred = sc.parallelize(sortedData.take(100))
+# Generamos una partición en el dataset con las 20 mejores peliculas,
+# generalmente entre 2-4 particiones en cada CPU
+best = sc.parallelize(sortedData.take(20))
+joinedDataTwenty = joinData(best, pairsMovie)
+joinedData = ordering(joinedDataTwenty, False)
 
-# Unimos las mejores peliculas mediante el movie_id
-# Genemos un RDD con movie_id, raiting, title
-joinedDataTwenty = joinData(bestTwenty, pairsMovie)
-joinedDataHundred = joinData(bestHundred, pairsMovie)
-# Ordenamos e imprimimos joinedData ordenado de forma descendente
+# -----------------------------------------------------------------------------------------------
+# b) Muestre cuáles son las 20 películas con mayor cantidad de ratings.
+# Mencione el tiempo en segundos que demora esta operación.
+t1 = time.time()
+joinedData.collect()
+print 'El tiempo de ejecución es %s segundos' % (str(time.time()-t1))
+# El tiempo de ejecución es 0.312572002411 segundos
 
-bestTwenty = generateData(joinedDataTwenty)
-bestHundred = generateData(joinedDataHundred)
-# El valor generado es
-# (movie_id, (countRating, title)
+# -----------------------------------------------------------------------------------------------
+# d) Usando la función filter de los RDD de Spark, obtenga sólo las películas con 100 o más ratings.
+bestRaiting = getBestRaiting(sortedData)
+filterBestRaiting = sc.parallelize(bestRaiting.take(bestRaiting.count()))
+joinedBestRaiting = joinData(filterBestRaiting, pairsMovie)
 
-print 'El tiempo de ejecución es ' + str(time.time() - t1) + ' segundos'
+# -> i ¿Cuál es el número total de las películas, y cuánto es el número de las películas filtradas?
+joinedBestRaiting.count()
 
-# El tiempo de ejecución es 3.43885302544 segundos
+# -> ii ¿Cuáles son las 10 películas con menor cantidad de ratings de las películas filtradas?
+worstRaiting = ordering(bestRaiting, True)
+worstRaiting = sc.parallelize(worstRaiting.take(10))
+joinedWorstRaiting = joinData(worstRaiting, pairsMovie)
+joinedWorstRaiting.collect()
+
+# -----------------------------------------------------------------------------------------------
+# e) Almacene en cache el RDD countRating, mediante la instrucción countRating.cache().
+# Vuelva a repetir el análisis de b), contando el tiempo en que demora la operación.
+best.cache()
+joinedDataTwenty = joinData(best, pairsMovie)
+joinedData = ordering(joinedDataTwenty, False)
+
+t1 = time.time()
+joinedData.collect()
+print 'El tiempo de ejecución es %s segundos' % (str(time.time()-t1))
+# El tiempo de ejecución es 0.271433115005 segundos
+# -----------------------------------------------------------------------------------------------
+
+# f) Efectúe el mismo análisis anterior entre b) y d), pero esta vez calculando el
+# rating promedio de cada película,
+#
+averagePairsRating = ratingsData.map(lambda vec: (vec[1], int(vec[2])))
+# ¿Qué operaciones debe efectuar después de pairsRatings para que se calcule el promedio de los ratings?
+averageData = averagePairsRating.groupByKey().mapValues(lambda x: sum(x) / len(x))
+bestAverageRaiting = ordering(averageData, False)
+
+# b) Muestre cuáles son las 20 películas con mayor cantidad de ratings.
+# Mencione el tiempo en segundos que demora esta operación.
+t = time.time()
+bestAverageRaiting.take(20)
+print 'El tiempo de ejecución es %s segundos' % (str(time.time()-t))
+# El tiempo de ejecución es 0.159805059433 segundos
+
+# d) Usando la función filter de los RDD de Spark, obtenga sólo las películas con 100 o más ratings.
+hundredAverageRaiting = getBestRaiting(bestAverageRaiting)
+tenWorseMovie = ordering(averageData, True)
+tenWorseMovie.take(10)
